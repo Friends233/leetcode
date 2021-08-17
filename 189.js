44f03590d42b0e98a18914bae29523720bdbf621
Friends233 <friends233@foxmail.com>
2021-08-17 17:42:03 +0800

    189. 旋转数组

---
+++ 189.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 189. 旋转数组
+ * @Author: Friends233
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+  k = k%nums.length
+  nums.unshift(...nums.splice(-k))
+};
